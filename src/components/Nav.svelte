27fdc2fb5af904df76f3fb07e1eec4ab03ccf368
jcamilofarfan <farfan1976@hotmail.com>
2022-02-07 17:06:58 -0500

    Rutas principales del blog: about, contact y tags

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -53,6 +53,7 @@
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
 		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
 		<li><a aria-current="{segment === 'contact' ? 'page': undefined}" href="contact">contact</a></li>
+    <li><a aria-current="{segment === 'general' ? 'page': undefined}" href="general">general</a></li>
 		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
 	</ul>
 </nav>
