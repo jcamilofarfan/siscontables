7c93ec571e54a6109763d547a8c2fdc17a74d307
jcamilofarfan <farfan1976@hotmail.com>
2022-02-08 16:08:39 -0500

    Estructura del blogpost con comentarios

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -14,42 +14,28 @@
 </script>
 
 <script>
+	import readingTime from "../../utils/readingTime";
+	import formatIsoTime from "../../utils/formatIsoTime"
 	export let post;
 </script>
 
 <style>
-	/*
-		By default, CSS is locally scoped to the component,
-		and any unused styles are dead-code-eliminated.
-		In this page, Svelte can't know which elements are
-		going to appear inside the {{{post.html}}} block,
-		so we have to use the :global(...) modifier to target
-		all elements inside .content
-	*/
-	.content :global(h2) {
-		font-size: 1.4em;
-		font-weight: 500;
-	}
-
-	.content :global(pre) {
-		background-color: #f9f9f9;
-		box-shadow: inset 1px 1px 5px rgba(0, 0, 0, 0.05);
-		padding: 0.5em;
-		border-radius: 2px;
-		overflow-x: auto;
-	}
-
-	.content :global(pre) :global(code) {
-		background-color: transparent;
+	h2{
+		color: #22215b;
+		font-size: 28px;
+		margin: 0;
 		padding: 0;
 	}
-
-	.content :global(ul) {
-		line-height: 1.5;
+	.Post-title{
+		color: #555;
+		font-size: 14px;
+		font-weight: 300;
+		margin-top: 5px;
+		padding: 0;
 	}
+	.comments{
+		margin: 2em 0 0 0;
 
-	.content :global(li) {
-		margin: 0 0 0.5em 0;
 	}
 </style>
 
@@ -57,8 +43,24 @@
 	<title>{post.title}</title>
 </svelte:head>
 
-<h1>{post.title}</h1>
-
-<div class="content">
-	{@html post.html}
+<div class="Post">
+	<div class="Post-title">
+		<h2>
+			{post.title}
+		</h2>
+	</div>
+	<p class="date">
+		<time datetime="{post.createdAt}">
+			📅 {formatIsoTime(
+				post.createdAt
+			)}
+		</time>
+		<span>
+			{readingTime(post.html)}
+		</span>
+	</p>
+	<div class="content">
+		{@html post.html}
+	</div>
+	<div class="comments" />
 </div>
