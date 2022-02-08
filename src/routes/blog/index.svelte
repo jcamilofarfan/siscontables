a551fc15074767a0018222f8e3d78f781d53f4e9
jcamilofarfan <farfan1976@hotmail.com>
2022-02-08 10:35:11 -0500

    Sección principal del blog y blogposts

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+	<title>Blog</title>
+</svelte:head>
+
 <script context="module">
 	export function preload() {
 		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
@@ -11,24 +15,13 @@
 </script>
 
 <style>
-	ul {
-		margin: 0 0 1em 0;
-		line-height: 1.5;
-	}
-</style>
 
-<svelte:head>
-	<title>Blog</title>
-</svelte:head>
+</style>
 
 <h1>Recent posts</h1>
 
 <ul>
 	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
 		<li><a rel="prefetch" href="blog/{post.slug}">{post.title}</a></li>
 	{/each}
 </ul>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,33 @@
+<script context="module">
+	export function preload() {
+		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
+
 <script>
-	import successkid from 'images/successkid.jpg';
+	import Post from "../components/Post.svelte"
+	export let posts;
 </script>
 
 <style>
-
+	.Posts{
+		display: grid;
+		justify-content: space-between;
+		grid-gap: 15px;
+		grid-template-columns: 1fr;
+	}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Jcamilofarfan - </title>
 </svelte:head>
 
 <div class="Home">
-	Index
+	<div class="Posts">
+		{#each posts as post}
+			<Post {post}/>
+		{/each}
+	</div>
 </div>
