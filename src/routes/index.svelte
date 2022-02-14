160d70e21feae8006d9676a6ae76b4d5040fadc1
jcamilofarfan <farfan1976@hotmail.com>
2022-02-14 13:03:26 -0500

    add meta all

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,25 +22,7 @@
 
 <svelte:head>
 	<title>Jcamilofarfan - </title>
-	<meta name="description" content="Blog Jcamilofarfan, software Developer and public Accountant"/>
-	<meta name="keywords" content="Jcamilofarfan, software Developer, public Accountant"/>
-	<meta name="author" content="Jcamilofarfan"/>
-	<meta name="twitter:card" content="summary_large_image"/>
-	<meta name="twitter:site" content="@jcamilofarfan"/>
-	<meta name="twitter:creator" content="@jcamilofarfan"/>
-	<meta name="twitter:title" content="Jcamilofarfan"/>
-	<meta name="twitter:description" content="Blog Jcamilofarfan, software Developer and public Accountant"/>
-	<meta name="twitter:image" content="https://contador.tech/wp-content/uploads/2021/12/Portada-Sistemas-Contables.png"/>
-	
-	<meta property="og:title" content="Jcamilofarfan"/>
-	<meta property="og:description" content="Blog Jcamilofarfan, software Developer and public Accountant"/>
-	<meta property="og:image" content="https://contador.tech/wp-content/uploads/2021/12/Portada-Sistemas-Contables.png"/>
-	<meta property="og:image:alt" content="Jcamilofarfan"/>
-	<meta property="og:url" content="https://blog.siscontables.dev/"/>
-	<meta property="og:site_name" content="Jcamilofarfan"/>
-	<meta property="og:locale" content="es_ES"/>
-	<meta property="og:type" content="article"/>
-	<meta property="fb:app_id" content="1085117785665000"/>
+
 
 </svelte:head>
 
