6622287d715e98a81927bc9d15947cd0bc4acfec
jcamilofarfan <farfan1976@hotmail.com>
2022-02-14 12:51:06 -0500

    Metadatos y optimización del SEO

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,6 +22,25 @@
 
 <svelte:head>
 	<title>Jcamilofarfan - </title>
+	<meta name="description" content="Blog Jcamilofarfan, software Developer and public Accountant"/>
+	<meta name="keywords" content="Jcamilofarfan, software Developer, public Accountant"/>
+	<meta name="author" content="Jcamilofarfan"/>
+	<meta name="twitter:card" content="summary_large_image"/>
+	<meta name="twitter:site" content="@jcamilofarfan"/>
+	<meta name="twitter:creator" content="@jcamilofarfan"/>
+	<meta name="twitter:title" content="Jcamilofarfan"/>
+	<meta name="twitter:description" content="Blog Jcamilofarfan, software Developer and public Accountant"/>
+	<meta name="twitter:image" content="https://contador.tech/wp-content/uploads/2021/12/Logo-Google-WorkSpace.png"/>
+	
+	<meta property="og:title" content="Jcamilofarfan"/>
+	<meta property="og:description" content="Blog Jcamilofarfan, software Developer and public Accountant"/>
+	<meta property="og:image" content="https://contador.tech/wp-content/uploads/2021/12/Logo-Google-WorkSpace.png"/>
+	<meta property="og:url" content="https://blog.siscontables.dev/"/>
+	<meta property="og:site_name" content="Jcamilofarfan"/>
+	<meta property="og:locale" content="es_ES"/>
+	<meta property="og:type" content="article"/>
+	<meta property="fb:app_id" content=""/>
+	
 </svelte:head>
 
 <div class="Home">
